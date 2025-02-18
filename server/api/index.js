33c81c783e084c6d6c816090533dfe1f3c2146e1
leomosley <leo.mosley2@gmail.com>
2025-02-18 14:25:32 +0000

    chore: config API server

--- server/api/index.js
+++ server/api/index.js
@@ -11,7 +11,7 @@ fastify.get('/', async function handler(request, reply) {
 
 // Run the server!
 try {
-  await fastify.listen({ port: 8080 });
+  await fastify.listen({ port: 3000 });
 } catch (err) {
   fastify.log.error(err);
   process.exit(1);
