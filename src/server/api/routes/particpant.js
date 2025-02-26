bada225aa1c9eed6e799d08b952484f6335ad28d
leomosley <leo.mosley2@gmail.com>
2025-02-26 12:17:44 +0000

    refactor: modular route handler functions

--- src/server/api/routes/particpant.js
+++ src/server/api/routes/particpant.js
@@ -1,56 +1,66 @@
 import { db } from '../../db/index.js';
 import { generateRandomId } from '../../../lib/utils.js';
 
+
+export async function getAllParticipants(request, reply) {
+  return await db.all('SELECT * FROM participant;');
+}
+
+export async function getParticipant(request, reply) {
+  const { id } = request.params;
+  const response = db.all(
+    `SELECT *
+       FROM 
+        participant AS p
+        JOIN race_participant AS rp USING (participant_id)
+      WHERE p.participant_id=?;`, id);
+
+  if (!response) throw new Error('Participant doesnt exist');
+
+  return response;
+}
+
+export async function createParticipant(request, reply) {
+  while (true) {
+    const id = generateRandomId();
+
+    const exists = await db.get('SELECT * FROM participant WHERE participant_id=?;', id);
+
+    if (!exists) {
+      await db.run('INSERT INTO participant (participant_id) VALUES (?);', id);
+
+      return { id };
+    }
+  }
+}
+
+export async function deleteParticipant(request, reply) {
+  const { id } = request.params;
+
+  return { message: `Delete race ${id}` };
+}
+
 export const participantRoutes = [
   {
     method: 'GET',
     url: '/api/participant',
-    handler: async (request, reply) => {
-      return await db.all('SELECT * FROM participant;');
-    },
+    handler: getAllParticipants,
   },
   {
     method: 'GET',
     url: '/api/participant/:id',
-    handler: async (request, reply) => {
-      const { id } = request.params;
-      const response = db.all(
-        `SELECT *
-         FROM 
-          participant AS p
-          JOIN race_participant AS rp USING (participant_id)
-        WHERE p.participant_id=?;`, id);
-
-      if (!response) throw new Error('Participant doesnt exist');
-
-      return response;
-    },
+    handler: getParticipant,
     requiredParams: ['id'],
   },
   {
     method: 'POST',
     url: '/api/participant',
-    handler: async (request, reply) => {
-      while (true) {
-        const id = generateRandomId();
-
-        const exists = await db.get('SELECT * FROM participant WHERE participant_id=?;', id);
-
-        if (!exists) {
-          await db.run('INSERT INTO participant (participant_id) VALUES (?);', id);
-
-          return { id };
-        }
-      }
-    },
+    handler: createParticipant,
   },
   {
     method: 'DELETE',
     url: '/api/participant/:id',
-    handler: async (request, reply) => {
-      const { id } = request.params;
-
-      return { message: `Delete race ${id}` };
-    },
+    handler: deleteParticipant,
+    requiredParams: ['id'],
   },
 ];
--- src/server/api/routes/race.js
+++ src/server/api/routes/race.js
@@ -1,92 +1,124 @@
 import { db } from '../../db/index.js';
 
+export async function getAllRaces(request, reply) {
+  return await db.all('SELECT * FROM race;');
+}
+
+export async function createRace(request, reply) {
+  const { location_id, race_name, race_date, check_in_open_time, race_start_time } = request.body;
+
+  const response = await db.run(
+    `INSERT INTO race (location_id, race_name, race_date, check_in_open_time, race_start_time) VALUES 
+      (?, ?, ?, ?, ?);`
+    , [location_id, race_name, race_date, check_in_open_time, race_start_time]);
+
+
+  if (!response) throw new Error('Error');
+
+  return response;
+}
+
+export async function getRace(request, reply) {
+  const { id } = request.params;
+
+  const raceResponse = await db.get('SELECT * FROM race WHERE race_id = ?;', [id]);
+
+  if (!raceResponse) throw new Error('Not found');
+
+  const locationResponse = await db.get('SELECT * FROM location WHERE location_id=?', [raceResponse.location_id]);
+
+  const checkpointsResponse = await db.all('SELECT * FROM checkpoint AS c JOIN race_checkpoint AS rc ON c.checkpoint_id = rc.checkpoint_id WHERE rc.race_id=?', [id]);
+
+  return {
+    ...raceResponse,
+    location: locationResponse,
+    checkpoints: checkpointsResponse,
+  };
+}
+
+export async function checkInParticipant(request, reply) {
+  const { id } = request.params;
+  const { participant_id } = request.body;
+
+  const response = await db.run(`
+      UPDATE race_participant
+      SET checked_in = TRUE
+      WHERE race_id = ? AND participant_id = ?;
+      `, [id, participant_id]);
+
+  if (!response) throw new Error('Not found');
+
+  return response;
+}
+
+export async function checkOutParticipant(request, reply) {
+  const { id } = request.params;
+  const { participant_id, end_time } = request.body;
+
+  const response = await db.run(`
+    UPDATE race_participant
+    SET end_time = ?
+    WHERE race_id = ? AND participant_id = ?;
+    `, [end_time, id, participant_id]);
+
+  if (!response) throw new Error('Not found');
+
+  return response;
+}
+
+export async function updateRace(request, reply) {
+  const { id } = request.params;
+  const body = request.body;
+
+  return { message: `Update race ${id}` };
+}
+
+export async function deleteRace(request, reply) {
+  const { id } = request.params;
+
+  return { message: `Delete race ${id}` };
+}
+
 export const raceRoutes = [
   {
     method: 'GET',
     url: '/api/race',
-    handler: async (request, reply) => {
-      return await db.all('SELECT * FROM race;');
-    },
+    handler: getAllRaces,
   },
   {
     method: 'POST',
     url: '/api/race',
-    handler: async (request, reply) => {
-      const { location_id, race_name, race_date, check_in_open_time, race_start_time } = request.body;
-
-      const response = await db.run(
-        `INSERT INTO race (location_id, race_name, race_date, check_in_open_time, race_start_time) VALUES 
-        (?, ?, ?, ?, ?);`
-        , [location_id, race_name, race_date, check_in_open_time, race_start_time]);
-
-
-      if (!response) throw new Error('Error');
-
-      return response;
-    },
+    handler: createRace,
     requiredParams: ['location_id', 'race_name', 'race_date', 'check_in_open_time', 'race_start_time'],
   },
   {
     method: 'GET',
     url: '/api/race/:id',
-    handler: async (request, reply) => {
-      const { id } = request.params;
-
-      const raceResponse = await db.get('SELECT * FROM race WHERE race_id = ?;', [id]);
-
-      if (!raceResponse) throw new Error('Not found');
-
-      const locationResponse = await db.get('SELECT * FROM location WHERE location_id=?', [raceResponse.location_id]);
-
-      const checkpointsResponse = await db.all('SELECT * FROM checkpoint AS c JOIN race_checkpoint AS rc ON c.checkpoint_id = rc.checkpoint_id WHERE rc.race_id=?', [id]);
-
-      return {
-        ...raceResponse,
-        location: locationResponse,
-        checkpoints: checkpointsResponse,
-      };
-    },
+    handler: getRace,
     requiredParams: ['id'],
   },
   {
     method: 'PATCH',
     url: '/api/race/:id/check-in',
-    handler: async (request, reply) => {
-      const { id } = request.params;
-      const { participant_id } = request.body;
-
-      const response = await db.run(`
-        UPDATE race_participant
-        SET checked_in = TRUE
-        WHERE race_id = ? AND participant_id = ?;
-        `, [id, participant_id]);
-
-      if (!response) throw new Error('Not found');
-
-      return response;
-    },
+    handler: checkInParticipant,
     requiredParams: ['id', 'participant_id'],
   },
   {
     method: 'PATCH',
+    url: '/api/race/:id/check-out',
+    handler: checkOutParticipant,
+    requiredParams: ['id', 'participant_id', 'end_time'],
+  },
+  {
+    method: 'PATCH',
     url: '/api/race/:id',
-    handler: async (request, reply) => {
-      const { id } = request.params;
-      const body = request.body;
-
-      return { message: `Update race ${id}` };
-    },
+    handler: updateRace,
     requiredParams: ['id'],
   },
   {
     method: 'DELETE',
     url: '/api/race/:id',
-    handler: async (request, reply) => {
-      const { id } = request.params;
-      const body = request.body;
-
-      return { message: `Delete race ${id}` };
-    },
+    handler: deleteRace,
     requiredParams: ['id'],
   },
 ];
