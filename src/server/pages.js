328d247ddb3915e33502a04f792fe5343b983599
leomosley <leo.mosley2@gmail.com>
2025-03-05 12:12:54 +0000

    feat(pages: volunteer finish page

--- src/server/pages.js
+++ src/server/pages.js
@@ -53,6 +53,23 @@ export async function participantPage(request, file) {
   return file;
 }
 
+export async function volunteerFinishPage(request, file) {
+  const { id } = request.params;
+
+  const response = await db.get('SELECT * FROM race WHERE race_id=?', id);
+
+  if (!response) throw new Error('Race doesnt exist');
+
+  file = file.replace(/{{id}}/g, id);
+  file = file.replace(/{{race_name}}/g, response.race_name);
+  file = file.replace(/{{race_date}}/g, response.race_date);
+  file = file.replace(/{{check_in_open_time}}/g, response.check_in_open_time);
+  file = file.replace(/{{race_start_time}}/g, response.race_start_time);
+  file = file.replace(/{{checkpoints}}/g, JSON.stringify(response.checkpoints));
+
+  return file;
+}
+
 export const pages = [
   { url: '/' },
   { url: '/organise' },
@@ -71,5 +88,12 @@ export const pages = [
   },
   { url: '/participant' },
   { url: '/participant/create' },
-  { url: '/participant/:id' },
+  {
+    url: '/participant/:id',
+    handler: participantPage,
+  },
+  {
+    url: '/volunteer/:id/finish',
+    handler: volunteerFinishPage
+  },
 ];
