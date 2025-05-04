098227cf51452853d04fd32e5d24d00d8ba0cb77
leomosley <leo.mosley2@gmail.com>
2025-05-04 15:19:13 +0100

    fix: import errors

--- src/client/components/auth/sign-up-view.js
+++ src/client/components/auth/sign-up-view.js
@@ -1,6 +1,6 @@
-import { userStore } from '../lib/auth.mjs';
-import { localStore } from '../lib/localStore.mjs';
-import { navigate, readPath } from '../lib/views.mjs';
+import { userStore } from '../../lib/auth.mjs';
+import { localStore } from '../../lib/localStore.mjs';
+import { navigate, readPath } from '../../lib/views.mjs';
 
 class SignUpView extends HTMLElement {
   constructor() {
