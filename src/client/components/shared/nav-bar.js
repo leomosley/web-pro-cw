86aaafecccfc67edbef50902c9717b19f5110f6e
leomosley <leo.mosley2@gmail.com>
2025-05-14 13:24:23 +0100

    feat: active navbar item state

--- src/client/components/shared/nav-bar.js
+++ src/client/components/shared/nav-bar.js
@@ -1,5 +1,6 @@
 import { templates } from '../../index.mjs';
 import { userStore } from '../../lib/auth.mjs';
+import { readPath } from '../../lib/views.mjs';
 
 // https://lucide.dev/icons/
 const homeIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-house-icon lucide-house"><path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8"/><path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/></svg>';
@@ -18,27 +19,43 @@ class NavBar extends HTMLElement {
 
     this.user = null;
     this.handleUserChange = this.handleUserChange.bind(this);
+    this.handleLocationChange = this.handleLocationChange.bind(this);
+    this.navButtons = [];
   }
 
   connectedCallback() {
     this.user = userStore.get();
     this.render();
+    this.updateActiveButton();
     this.unsubscribe = userStore.watch(this.handleUserChange);
+    window.addEventListener('popstate', this.handleLocationChange);
+    window.addEventListener('view-changed', this.handleLocationChange);
   }
 
   disconnectedCallback() {
-    if (this.unsubscribe) this.unsubscribe();
+    if (this.unsubscribe) {
+      this.unsubscribe();
+    }
+    window.removeEventListener('popstate', this.handleLocationChange);
+    window.removeEventListener('view-changed', this.handleLocationChange);
   }
 
   handleUserChange(newUserValue) {
     this.user = newUserValue;
     this.render();
+    this.updateActiveButton();
+  }
+
+  handleLocationChange() {
+    this.updateActiveButton();
   }
 
   generateNav() {
     const nav = [{ label: 'Home', view: 'home', icon: homeIcon }];
 
-    if (!this.user) nav.push({ label: 'Sign In', view: 'sign-in', icon: loginIcon });
+    if (!this.user) {
+      nav.push({ label: 'Sign In', view: 'sign-in', icon: loginIcon });
+    }
 
     switch (this.user?.role) {
       case 'participant': nav.push({ label: 'Participant Page', view: 'participant', icon: participantIcon }); break;
@@ -57,7 +74,7 @@ class NavBar extends HTMLElement {
   render() {
     const nav = this.generateNav();
     const navbar = this.shadowRoot.querySelector('.navbar');
-
+    this.navButtons = [];
     navbar.innerHTML = '';
 
     for (const item of nav) {
@@ -65,7 +82,22 @@ class NavBar extends HTMLElement {
       navButton.textContent = item.label;
       navButton.innerHTML += item.icon;
       navButton.setAttribute('view', item.view);
+
+      this.navButtons.push(navButton);
       navbar.append(navButton);
+    }
+  }
+
+  updateActiveButton() {
+    const currentPath = readPath();
+
+    for (const button of this.navButtons) {
+      const buttonView = button.getAttribute('view');
+      if (currentPath.startsWith(buttonView)) {
+        button.classList.add('active');
+      } else {
+        button.classList.remove('active');
+      }
     }
   }
 }
